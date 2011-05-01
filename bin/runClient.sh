0f87cdc9eb6383a905d61be7e164b21c59a2d9d3
Daniel Glauser <danglauser@gmail.com>
2011-05-01 10:27:20 -0600

    Found the major bug in the shell scripts, call the code with java instead of scala since the scala-library.jar is in the deps directory.

--- bin/runClient.sh
+++ bin/runClient.sh
@@ -4,4 +4,4 @@ for jar in `ls ./deps/*.jar`; do
   LOCAL_CLASSPATH=${LOCAL_CLASSPATH}:${jar}
 done
 
-scala -cp $LOCAL_CLASSPATH actorproto.Client
+java -cp $LOCAL_CLASSPATH -Dakka.config="./src/main/resources/proto-akka.conf" actorproto.Client
--- bin/runServer.sh
+++ bin/runServer.sh
@@ -4,4 +4,5 @@ for jar in `ls ./deps/*.jar`; do
   LOCAL_CLASSPATH=${LOCAL_CLASSPATH}:${jar}
 done
 
-scala -cp $LOCAL_CLASSPATH -Dakka.config="./src/main/resources/proto-akka.conf" actorproto.Server
+#java -cp $LOCAL_CLASSPATH -Dakka.config="/Users/dglauser/Projects/scala/akka/config/akka.conf" actorproto.Server
+java -cp $LOCAL_CLASSPATH -Dakka.config="./src/main/resources/proto-akka.conf" actorproto.Server
