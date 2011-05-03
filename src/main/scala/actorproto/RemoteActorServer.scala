80d0a4905a3d0762b41781d4ee20a3918030d159
Daniel Glauser <danglauser@gmail.com>
2011-05-02 23:42:30 -0600

    Added additional perf stats, still needs some work (see notes in the code).

--- src/main/scala/actorproto/RemoteActorServer.scala
+++ src/main/scala/actorproto/RemoteActorServer.scala
@@ -42,16 +42,16 @@ class ClientActor(dataCollector: ActorRef, remediator: ActorRef) extends Actor {
   def receive = {
     case message @ "collect" =>
       println(name + " Sending \"" + message + "\" -> to the " + dataCollector.id) 
-      dataCollector !! message
+      timed(printTime(name + " " + message + " message sent in ")) { dataCollector ! message }
     case message @ "collect registry" =>
       println(name + " Sending \"" + message + "\" -> to the " + dataCollector.id) 
-      dataCollector !! message
+      timed(printTime(name + " " + message + " message sent in ")) { dataCollector ! message }
     case message @ "simple remediation" =>
       println(name + " Sending \"" + message + "\" -> to the " + remediator.id)
-      remediator !! message
+      timed(printTime(name + " " + message + " message sent in ")) { remediator ! message }
     case message @ "complex remediation" =>
       println(name + " Sending \"" + message + "\" -> to the " + remediator.id)
-      remediator !! message
+      timed(printTime(name + " " + message + " message sent in ")) { remediator ! message }
     case message @ "ACK" =>
       println("Client: received ACK")
   }
@@ -84,13 +84,15 @@ object Client {
   val name = "Client: "
   val collectionMessage = "collect"
   val remediationMessage = "simple remediation"
-  val MULTIPLIER = 10
+  val MULTIPLIER = 100
   var messages = List("")
   
   def sendManyMessages = {
     val dataCollector = Actor.remote.actorFor("data-collection-service", "localhost", 2552)
     val remediator = Actor.remote.actorFor("remediation-service", "localhost", 2552)
     val client = Actor.actorOf( new ClientActor(dataCollector, remediator) ).start
+    
+    var perfInfo: Map[String, Long] = Map("startTime" -> System.nanoTime)
         
     val collectionMessages = List("collect registry", "collect")
     messages = List.tabulate(MULTIPLIER)(count => collectionMessages.apply(count % collectionMessages.length))        
@@ -100,6 +102,9 @@ object Client {
       }
     }
     
+    perfInfo = perfInfo + ("endTimeCollections" -> System.nanoTime)
+    perfInfo = perfInfo + ("numCollections" -> messages.length)
+    
     val remediationMessages = List("simple remediation", "complex remediation")
     // Create a List of messages by repeating the original List
     messages = List.tabulate(MULTIPLIER)(count => remediationMessages.apply(count % remediationMessages.length))    
@@ -108,6 +113,32 @@ object Client {
         client ! message
       }
     }
+    
+    perfInfo = perfInfo + ("endTimeComplexRemediations" -> System.nanoTime)
+    perfInfo = perfInfo + ("numComplexRemediations" -> messages.length)
+    
+    // Otherwise this prints out too soon since the IO for the actors happens in a different thread
+    Thread.sleep(1500)
+    printPerformanceStats(perfInfo)
+    
+    // Need to figure out how to wait of all the threads to complete
+    // Need to figure out why the ACKs don't show up
+  }
+  
+  def printPerformanceStats(perfInfo : Map[String, Long]) = {
+    println("------------------------------------------------------")
+    println("                    Summary")
+    println("------------------------------------------------------")
+    println("")
+    
+    val collectionTime : Long = perfInfo.get("endTimeCollections").get - perfInfo.get("startTime").get
+    val collectionRate : Double = perfInfo.get("numCollections").get / (collectionTime.toDouble / 1000000000)
+    
+    println("Sent " + perfInfo.get("numCollections").get + " collections at a rate of " + collectionRate + " per second")
+    println("Over a total of " + (collectionTime/1000000).toDouble + " milliseconds")
+    
+    println("\n")
+    println("------------------------------------------------------")
   }
   
   def run = sendManyMessages
