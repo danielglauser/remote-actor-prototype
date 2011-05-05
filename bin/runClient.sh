35ce40b565dd08115aa534074996d13cb208787b
Daniel Glauser <danglauser@gmail.com>
2011-05-05 13:48:31 -0600

    Now use PROTO_JAVA_OPS for globally set JVM parameters.

--- bin/runClient.sh
+++ bin/runClient.sh
@@ -4,4 +4,5 @@ for jar in `ls ./deps/*.jar`; do
   LOCAL_CLASSPATH=${LOCAL_CLASSPATH}:${jar}
 done
 
-java -cp $LOCAL_CLASSPATH -Dakka.config="./src/main/resources/proto-akka.conf" actorproto.Client
+PROTO_JAVA_OPS=$PROTO_JAVA_OPS' ''-Dakka.config=./src/main/resources/proto-akka.conf'
+java -cp $LOCAL_CLASSPATH $PROTO_JAVA_OPS actorproto.Client
--- bin/runServer.sh
+++ bin/runServer.sh
@@ -4,5 +4,5 @@ for jar in `ls ./deps/*.jar`; do
   LOCAL_CLASSPATH=${LOCAL_CLASSPATH}:${jar}
 done
 
-#java -cp $LOCAL_CLASSPATH -Dakka.config="/Users/dglauser/Projects/scala/akka/config/akka.conf" actorproto.Server
-java -cp $LOCAL_CLASSPATH -Dakka.config="./src/main/resources/proto-akka.conf" actorproto.Server
+PROTO_JAVA_OPS=$PROTO_JAVA_OPS' ''-Dakka.config=./src/main/resources/proto-akka.conf'
+java -cp $LOCAL_CLASSPATH $PROTO_JAVA_OPS actorproto.Server
